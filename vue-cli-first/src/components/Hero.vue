06055df065dfeff92b508b86cb23737339cf293c
pyroYV <simonerizzo.pvt@gmail.com>
2022-07-15 13:55:59 +0200

    mile and bonus

---
+++ vue-cli-first/src/components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero" id="jumbotronz">
+    <img src="../assets/4jsfop.gif"/>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'NomeProvaComponente',
+    props: {
+    msg: String
+  }
+
+}
+</script>
+
+<style>
+
+</style>
